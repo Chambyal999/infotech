827944f93ebf52dc44e461fcf6c6893da3a87f3c
Atul <chambyalatul34@gmail.com>
2025-09-15 18:14:34 +0530

    new changes
    new changes

--- wp-content/themes/Leela Theme/front-page.php
+++ wp-content/themes/Leela Theme/front-page.php
@@ -11,7 +11,7 @@ get_header();
     
       <swiper-container class="mySwiper" pagination="true" pagination-clickable="true" navigation="true" space-between="30"
     centered-slides="true" autoplay-delay="2500" autoplay-disable-on-interaction="false">
-    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide> </swiper-slide>
     <swiper-slide>Slide 2</swiper-slide>
     <swiper-slide>Slide 3</swiper-slide>
     <swiper-slide>Slide 4</swiper-slide>
