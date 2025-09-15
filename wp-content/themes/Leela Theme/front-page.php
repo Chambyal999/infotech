3f27a6c81d8161c16d5873253376fa95eff66118
Atul <chambyalatul34@gmail.com>
2025-09-15 17:56:17 +0530

    new changes
    new changes

--- wp-content/themes/Leela Theme/front-page.php
+++ wp-content/themes/Leela Theme/front-page.php
@@ -8,16 +8,22 @@ get_header();
 ?>
 <main id="main" class="site-main">
       <div class="header-background">
-                    <div class="text">
-                        <h2>We Design and Develop</h2>
-                        <p>
-                            We are a new design studio based in USA. We have
-                            over 20 years of combined experience, and know a
-                            thing or two about designing websites and mobile
-                            apps
-                        </p>
-                        <button>contact us</button>
-                    </div>
+    
+      <swiper-container class="mySwiper" pagination="true" pagination-clickable="true" navigation="true" space-between="30"
+    centered-slides="true" autoplay-delay="2500" autoplay-disable-on-interaction="false">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide>
+    <swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide>
+    <swiper-slide>Slide 9</swiper-slide>
+  </swiper-container>
+
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-element-bundle.min.js"></script>
+
                 </div>
 
 
