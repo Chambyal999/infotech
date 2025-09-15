900884f1f3d62866edfdbabd75a080b59970409f
Atul <chambyalatul34@gmail.com>
2025-09-15 23:50:59 +0530

    new changes
    new changes

--- wp-content/themes/Leela Theme/front-page.php
+++ wp-content/themes/Leela Theme/front-page.php
@@ -9,7 +9,14 @@ get_header();
 
 
 <main id="main" class="site-main">
-  
+    <div class="container">
+        <div class="card" data-tilt>
+          
+            <h2>UI/UX Design</h2>
+            <p style="font-size: 12 !important;">Elevate Your User Experience with GoatCode’s Expert UI/UX Design Services Crafting Intuitive and Engaging Digital Experiences for Your Audience At GoatCode, we specialize in creating</p>
+            <a href="mailto:geekyprashant840@gmail.com" target="_blank" class="btn">Read More</a>
+        </div>
+    </div>
 <?php 
   // Hero Banner
   get_template_part('/template-parts/header/hero-banner');
