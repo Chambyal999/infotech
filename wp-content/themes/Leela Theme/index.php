424aac909da2eca16065c3e0d64881199cc80781
Atul <chambyalatul34@gmail.com>
2025-09-17 07:01:28 +0530

    new button added
    new button added  for 3 pages only

--- wp-content/themes/Leela Theme/index.php
+++ wp-content/themes/Leela Theme/index.php
@@ -8,13 +8,15 @@ get_header();
 ?>
 
 <div id="primary">
-    <main id="main" class="site-main mt-5" role="main">
-    <?php if( have_posts() ){     
+    <main id="main" class="site-main mt-5 container" role="main">
+    <div class="p-5">
+        <?php if( have_posts() ){     
          while ( have_posts() ) : the_post();  
          the_content();
     endwhile;
     }
     ?>
+    </div>
     </div>
     </main>
 </div>
